5470493300b39c588de6ecec63f26de0db6a70bc
Ramesh Kaushika De Silva <149869212+Ramesh-Kaushika@users.noreply.github.com>
2024-01-19 16:59:24 +0530

    Login Done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Login from "./pages/Login/Login.jsx";
+
+function App() {
+
+
+  return (
+      <div className={'myStyle'}>
+              <Login/>
+
+      </div>
+  )
+}
+
+export default App
